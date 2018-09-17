40bd810f4b92640593ab041a9dce27e18fe97598
prakher <prakher1992@gmail.com>
2018-09-17 08:50:41 +0530

    first commit

---
+++ blog/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+from .models import Post
+
+
+def home(request):
+    context={
+        'posts':Post.objects.all(),
+        'title':'home'
+    }
+    return render(request,'blog/home.html',context)
+
+def about(request):
+    context = {
+        'title': 'About'
+    }
+    return render(request,'blog/about.html',context)
+
